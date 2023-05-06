28fc01052802227345fea05f99585d444743e250
unknown <ghkim7319@gmail.com>
2023-05-06 16:13:56 +0900

    [setting] initial setting

---
+++ src/main/java/com/example/gardengenie/GardenGenieApplication.java
@@ -0,0 +1,13 @@
+package com.example.gardengenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenGenieApplication.class, args);
+	}
+
+}
