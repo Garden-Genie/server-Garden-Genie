75f5155770038de5e725608d40f0667c0a1e25f1
U_Zin <930yujin@gmail.com>
2023-06-06 22:03:59 +0900

    [feat] ChatGPT - mapping pltName to body

--- GardenGenie/src/main/java/com/example/gardengenie/controller/ChatController.java
+++ GardenGenie/src/main/java/com/example/gardengenie/controller/ChatController.java
@@ -1,25 +1,33 @@
-//
 package com.example.gardengenie.controller;
 
 import com.example.gardengenie.dto.ChatGptResponseDto;
 import com.example.gardengenie.dto.QuestionRequestDto;
+import com.example.gardengenie.repository.PlantRepository;
 import com.example.gardengenie.service.ChatService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat-gpt")
 public class ChatController {
     private final ChatService chatService;
+    private final PlantRepository plantRepository;
 
-    public ChatController(ChatService chatService) {
+    public ChatController(ChatService chatService, PlantRepository plantRepository) {
         this.chatService = chatService;
+        this.plantRepository = plantRepository;
     }
 
     @PostMapping("/question")
-    public ChatGptResponseDto sendQuestion(@RequestBody QuestionRequestDto requestDto) {
+    public ChatGptResponseDto sendQuestion() {
+        String pltName = plantRepository.findMostRecentPltName();
+        QuestionRequestDto requestDto = new QuestionRequestDto();
+        requestDto.setBody(pltName);
         return chatService.askQuestion(requestDto);
+    }
+
+    @PostMapping("/ask/poem")
+    public ChatGptResponseDto askPoem(@RequestBody QuestionRequestDto requestDto) {
+        return chatService.askPoem(requestDto);
+
     }
 }
