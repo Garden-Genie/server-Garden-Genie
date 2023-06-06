af6f5108cab9b740fa3d9d7928722b24a5f97cc7
U_Zin <930yujin@gmail.com>
2023-06-06 22:07:52 +0900

    [feat] ChatGPT - Repeated comment processing

--- GardenGenie/src/main/java/com/example/gardengenie/config/ChatGptConfig.java
+++ GardenGenie/src/main/java/com/example/gardengenie/config/ChatGptConfig.java
@@ -3,7 +3,7 @@ package com.example.gardengenie.config;
 public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "MY_API_KEY";
+    public static final String API_KEY = "MY_API";
     public static final String MODEL = "text-davinci-003";
 //    public static final String MODEL = "gpt-3.5-turbo";
     public static final Integer MAX_TOKEN = 500;
@@ -11,6 +11,9 @@ public class ChatGptConfig {
     public static final Double TOP_P = 1.0;
     public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
     public static final String URL = "https://api.openai.com/v1/completions";
-    public static final String REPEATING_1TEXT ="Please explain the plant";
-    public static final String REPEATING_2TEXT ="within 2 lines in Korean.";
+    public static final String NAME_1TEXT ="Please explain the plant";
+    public static final String NAME_2TEXT ="within 2 lines in Korean.";
+
+    public static final String POEM_1TEXT ="Create a poem about a plant ";
+    public static final String POEM_2TEXT ="Answer me in Korean. ";
 }
