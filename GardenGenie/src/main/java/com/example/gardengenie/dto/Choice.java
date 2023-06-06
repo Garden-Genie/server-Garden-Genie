33b484f9d380c7ca444c48ff2231f68e6229dd11
U_Zin <930yujin@gmail.com>
2023-06-07 04:06:12 +0900

    [feat] ChatGPT - change to gpt-3.5-turbo : response body

--- GardenGenie/src/main/java/com/example/gardengenie/dto/Choice.java
+++ GardenGenie/src/main/java/com/example/gardengenie/dto/Choice.java
@@ -11,14 +11,28 @@ import java.io.Serializable;
 @NoArgsConstructor
 public class Choice implements Serializable {
 
-    private String text;
+    private Message message;
     private Integer index;
     @JsonProperty("finish_reason")
     private String finishReason;
 
+    public String getContent() {
+        if (message != null && message.getContent() != null) {
+            return message.getContent();
+        }
+        return null;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class Message {
+        private String role;
+        private String content;
+    }
+
     @Builder
-    public Choice(String text, Integer index, String finishReason) {
-        this.text = text;
+    public Choice(Message message, Integer index, String finishReason) {
+        this.message = message;
         this.index = index;
         this.finishReason = finishReason;
     }
