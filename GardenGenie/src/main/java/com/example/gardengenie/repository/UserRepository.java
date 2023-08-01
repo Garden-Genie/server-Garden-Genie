b8256d81a335de4432bc0936243568771598a42b
U_Zin <930yujin@gmail.com>
2023-08-02 02:22:16 +0900

    [fix] UserRepository type change String to Long

--- GardenGenie/src/main/java/com/example/gardengenie/repository/UserRepository.java
+++ GardenGenie/src/main/java/com/example/gardengenie/repository/UserRepository.java
@@ -4,6 +4,8 @@ package com.example.gardengenie.repository;
 import com.example.gardengenie.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, String>{
+//public interface UserRepository extends CrudRepository<User, String>{}
 
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUserId(String userId);
 }
