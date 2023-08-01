84edcf109ea327a544238503ac9eef70fb3bb40d
U_Zin <930yujin@gmail.com>
2023-08-02 02:22:57 +0900

    [feat] Story table insert

--- GardenGenie/src/main/java/com/example/gardengenie/controller/StoryController.java
+++ GardenGenie/src/main/java/com/example/gardengenie/controller/StoryController.java
@@ -16,13 +16,26 @@ public class StoryController {
     private StoryRepository storyRep;
 
     @Autowired
-    public StoryController(StoryRepository storyRep){this.storyRep = storyRep;}
+    public StoryController(StoryRepository storyRep){
+        this.storyRep = storyRep;
+    }
+
+//    @PostMapping
+//    public Story put(@RequestParam int storyId, @RequestParam User user, @RequestParam Plant plant, @RequestParam String pltName, @RequestParam LocalDateTime storyDate,
+//                     @RequestParam String storyExplain, @RequestParam String storyMusic, @RequestParam String storyPoem, @RequestParam String storyCondition){
+//        return storyRep.save(new Story(storyId, plant, pltName, storyDate, storyExplain, storyMusic, storyPoem, storyCondition));
+//    }
 
     @PostMapping
-    public Story put(@RequestParam int storyId, @RequestParam User user, @RequestParam Plant plant, @RequestParam String pltName, @RequestParam LocalDateTime storyDate,
-                     @RequestParam String storyExplain, @RequestParam String storyMusic, @RequestParam String storyPoem, @RequestParam String storyCondition){
-        return storyRep.save(new Story(storyId, user, plant, pltName, storyDate, storyExplain, storyMusic, storyPoem, storyCondition));
+    public Story put(@RequestBody Story story) {
+        // story_id는 auto increment로 생성됨
+        // story_date는 현재 시간으로 설정
+        story.setStory_id(0); // 0 또는 null로 설정하면 DB에서 자동 생성됨
+        story.setStory_date(LocalDateTime.now());
+
+        return storyRep.save(story);
     }
+
 
     @GetMapping
     public Iterable<Story> list(){return storyRep.findAll();}
